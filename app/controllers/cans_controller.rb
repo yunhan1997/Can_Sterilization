624740721bfa8ee02252a308daae138fe27eb2aa
han <victory0246@gmail.com>
2022-12-28 16:05:50 +0800

    index upload csv, result shows csv

--- app/controllers/cans_controller.rb
+++ app/controllers/cans_controller.rb
@@ -1,4 +1,17 @@
+require 'csv'
+
 class CansController < ApplicationController
     def index
+    end
+
+
+    def result
+        rowarray = Array.new
+        myfile = params[:file]
+    
+        CSV.foreach(myfile.path) do |row|
+          rowarray << row
+          @rowarraydisp = rowarray
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get "cans", to: "cans#index"
+  post '/cans/result', to: "cans#result"
+
   resources :cans
+
+  root "cans#index"
 end
